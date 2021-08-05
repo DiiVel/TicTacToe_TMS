20232b4758314f1a1293ff401eece714edd49b03
Denis <krapivnitsky1999@mail.ru>
2021-08-05 18:47:47 +0300

    "background start resolution"

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame as pg
+from play.colors import Color
+
+timer = pg.time.Clock()
+fps = 120
+pg.init()
+game_screen = pg.display.set_mode(
+    (800, 600),
+    pg.HWSURFACE | pg.DOUBLEBUF | pg.OPENGL,
+)
+pg.display.set_caption("Tic Tac Toe")
+game_screen.fill(Color.LIGHT_GRAY)
+work = True
+while work:
+    keys = pg.key.get_pressed()
+    work = not keys[pg.K_ESCAPE]
+    timer.tick(fps)
+    events = pg.event.get()
+    for ev in events:
+        print(ev)
+        if ev.type == pg.WINDOWCLOSE:
+            work = False
+
+pg.quit()
---
+++ play/colors.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Color(tuple, Enum):
+    WHITE = (255, 255, 255)
+    LIGHT_GRAY = (112, 112, 112)
